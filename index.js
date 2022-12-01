a64ac1df76fa7fb0ebaa9ea8d72b4da6d52deb3a
olivia <opierez@gmail.com>
2022-11-30 23:09:14 -0500

    commit tree 574376306a77ded5a30b7cf7b4e5c10abb6ed726

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        }),
+    })
+    .then(resp => resp.json())
+    .then(userInfo => {
+        let user = userInfo.id 
+        let div = document.createElement('div')
+        div.textContent = user
+        document.querySelector('body').appendChild(div)
+    })
+    .catch(error => document.body.innerHTML = error.message)
+};
+
+
